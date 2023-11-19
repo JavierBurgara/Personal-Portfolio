dc1f8e47facbfaad218b64b441e423b131ff7f14
javierburgara <javierburgara150@yahoo.com>
2023-11-18 20:02:05 -0800

    added contact me form and projects

--- Personal-Portfolio/src/components/ContactMe/ContactMe.jsx
+++ Personal-Portfolio/src/components/ContactMe/ContactMe.jsx
@@ -2,8 +2,55 @@ import "./ContactMe.css";
 
 export function ContactMe() {
   return (
-    <div>
-      <h1 className="h1">Contact Me</h1>
+    <form className="wow pulse" action="https://yahoo.com/javierburgara150@yahoo.com" method="POST">
+    <div className="form-group">
+        <div className="row">
+            <div className="col-12 col-sm-12 col-md-6 mx-auto">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control form-control-lg"
+                    id="name"
+                    placeholder="Name"
+                />
+            </div>
+        </div>
     </div>
+    <div className="form-group hidden">
+        <div className="row">
+            <div className="col-12 col-sm-12 col-md-6 mx-auto">
+                <input
+                    type="email"
+                    name="_replyto"
+                    className="form-control form-control-lg"
+                    id="exampleFormControlInput1"
+                    placeholder="Your email"
+                />
+            </div>
+        </div>
+    </div>
+    <div className="form-group hiddenRight">
+        <div className="row">
+            <div className="col-12 col-sm-12 col-md-6 mx-auto">
+                <textarea
+                    name="message"
+                    className="form-control form-control-lg"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    placeholder="Write your message..."
+                />
+            </div>
+        </div>
+    </div>
+    <div className="row text-md-right text-sm-center">
+        <div className="col-12 col-sm-12 col-md-6 mx-auto">
+            <button
+                type="submit"
+                className="btn btn-primary mb-2 hidden">
+                Submit
+            </button>
+        </div>
+    </div>
+</form>
   );
 }
