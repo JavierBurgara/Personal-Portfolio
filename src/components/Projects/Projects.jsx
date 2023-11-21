bcf77fb66c1bd6e9b77034e51772e433d27e19a3
JavierBurgara <javierburgara150@yahoo.com>
2023-11-20 23:13:29 -0800

    added css to header

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -21,12 +21,33 @@ export function Projects() {
             operations such as creating events, retrieving event details,
             updating event information, and managing calendars.
           </p>
-          <a className="proj-button" href="https://protected-taiga-58244-3710fb729053.herokuapp.com/" target="_blank" rel="noreferrer">Show more</a>
+          <a
+            className="proj-button"
+            href="https://protected-taiga-58244-3710fb729053.herokuapp.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Show more
+          </a>
         </div>
-        <div className="card-container">
-          <p>Card Title</p>
-          <p>Card Picture</p>
-          <p>Card Description</p>
+        <div
+          className="card-container"
+          href="https://julie0327.github.io/trails_weather/"
+        >
+          <p>Trails&Weather</p>
+          <img
+            src=".\src\media\Screenshot 2023-11-20 182210.jpg"
+            alt="Api website"
+            className="card-img"
+          />
+          <p>
+            How to use OpenWeather API to retrieve weather from several
+            different cities in the United States. We use the OpenWeather API to
+            retrieve Data from its weather to input what kind of weather
+            conditions are going to happen in the next 5 days. This helps the
+            users plan their next oncomings days to aviod any weather conditions
+            that could effect their plans those next 5 days.
+          </p>
           <a className="proj-button">Show more</a>
         </div>
         <div className="card-container">
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,13 +6,6 @@ export function Skills() {
       <h1 className="h1-skills">Skills</h1>
       <div className="skill-lists">
         <ul>
-          <p className="skills-list-header">Programming Languages</p>
-          <li>JavaScript</li>
-          <li>HTML</li>
-          <li>CSS3</li>
-        </ul>
-     
-        <ul> 
           <p className="skills-list-header">Frontend</p>
           <li>React.js</li>
           <li>Redux</li>
@@ -27,34 +20,24 @@ export function Skills() {
           <li>Node.js</li>
           <li>Express.js</li>
           <li>RESTful API Development</li>
-          <li>Object-Relational Mapping (ORM)</li>
-          <li>MVC (Model-View-Controller) Architecture</li>
+          <li>Object-Relational Mapping</li>
+          <li>MVC (Model-View-Controller)</li>
           <li>Third-Party APIs</li>
           <li>Object-Oriented Programming(OOP)</li>
           <li>Server-Side APIs</li>
           <li>Web APIs</li>
-        </ul>
-
-        <ul>
-          <p className="skills-list-header">Database</p>
           <li>MongoDB</li>
           <li>MYSQL</li>
           <li>NOSQL</li>
         </ul>
 
         <ul>
-          <p className="skills-list-header">Version Control</p>
+          <p className="skills-list-header">Miscellaneous Tools</p>
           <li>Git</li>
           <li>GitHub</li>
           <li>GitLabs</li>
         </ul>
-        
-        <ul>
-          <p className="skills-list-header">Tools</p>
-          <li>Npm</li>
-        </ul>
       </div>
-    
     </div>
   );
 }
